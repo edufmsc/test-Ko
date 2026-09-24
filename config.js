37182f92ce3e0cfbb8ed9fa5288a146d2918fd4e
edufmsc <edu.fmsc@gmail.com>
2026-09-24 13:23:06 +0800

    Update AI_LMS_CONFIG with new Google client ID

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 window.AI_LMS_CONFIG = {
-  STORAGE_PREFIX: 'ai-learning-frontend-v1:'
+  GOOGLE_CLIENT_ID: 'AKfycbxfD3vkA2ndUTFAvkzSffsRXD79yE1DgCcKr9tIteQHvr0TRa9mz8VSVkRhgRplgVjU',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxfD3vkA2ndUTFAvkzSffsRXD79yE1DgCcKr9tIteQHvr0TRa9mz8VSVkRhgRplgVjU/exec',
+  STORAGE_PREFIX: 'ai-learning-sync-v1:'
 };
