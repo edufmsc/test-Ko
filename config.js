52d4da8098594b0958760ea581b1cfc0c6bc8870
edufmsc <edu.fmsc@gmail.com>
2026-09-24 14:06:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.AI_LMS_CONFIG = {
-  GOOGLE_CLIENT_ID: 'AKfycbxfD3vkA2ndUTFAvkzSffsRXD79yE1DgCcKr9tIteQHvr0TRa9mz8VSVkRhgRplgVjU',
+  GOOGLE_CLIENT_ID: '731637287753-diajenfrr7p0bs43isijf1t40u7a07m6.apps.googleusercontent.com',
   APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxfD3vkA2ndUTFAvkzSffsRXD79yE1DgCcKr9tIteQHvr0TRa9mz8VSVkRhgRplgVjU/exec',
   STORAGE_PREFIX: 'ai-learning-sync-v1:'
 };
